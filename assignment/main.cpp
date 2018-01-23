1f2083af30ff6c1b79dad5e4ba45c769cee06fb7
Hibbs <hibbscm@msoe.edu>
2018-01-22 21:02:27 -0600

    Added semaphores and thread logic

--- assignment/main.cpp
+++ assignment/main.cpp
@@ -5,6 +5,8 @@
 #include <queue>
 #include <mutex>
 
+#include <semaphore.h>
+
 using namespace std;
 
 typedef enum OP {PLUS, MINUS, MULTIPLY, DIVIDE};
@@ -13,14 +15,22 @@ typedef struct Equation {
     int a;
     int b;
     OP op;
+    long answer;
 };
 
-queue pool;
+queue<Equation> pool;
 
 mutex created_lock, consumed_lock;
 
+sem_t enqueue_semaphore;
+
 int operations_created = 0;
 int operations_consumed = 0;
+
+
+// Function Definitions
+void evaluate(Equation*);
+char get_symbol(OP);
 
 void producer_thread() {
     int created = 0;
@@ -30,9 +40,16 @@ void producer_thread() {
         int b = rand();
         OP op = (OP) rand() % 4;
 
+        // Create a new equation
         Equation eq = Equation{a, b, op};
 
+        // Wait for an opening in the queue
+        sem_wait(&enqueue_semaphore);
 
+        // Add the equation to the work queue
+        pool.push(eq);
+
+        // Increment the global scope variables
         created_lock.lock();
         created++;
         operations_created++;
@@ -40,8 +57,23 @@ void producer_thread() {
     }
 }
 
-void consumer_thread(Equation* eq) {
+void consumer_thread() {
+    int consumed = 0;
 
+    while(true) {
+        Equation eq = pool.front();
+        sem_post(&enqueue_semaphore);
+
+        char c = get_symbol(eq.op);
+        evaluate(&eq);
+        printf("%d %c %d = %d", eq.a, c, eq.b, eq.answer);
+        // TODO stuff
+
+        consumed_lock.lock();
+        consumed++;
+        operations_consumed++;
+        consumed_lock.unlock();
+    }
 }
 
 void producer_manager(int producers) {
@@ -52,7 +84,26 @@ void consumer_manager(int consumers) {
     cout << "Consumer Manager" << endl;
 }
 
+char get_symbol(OP op) {
+    switch(op) {
+        case PLUS:     return '+';
+        case MINUS:    return '-';
+        case MULTIPLY: return '*';
+        case DIVIDE:   return '/';
+    }
+}
 
+void evaluate(Equation* eq) {
+    switch(eq->op) {
+        case PLUS: eq->answer = eq->a + eq->b;
+            break;
+        case MINUS: eq->answer = eq->a - eq->b;
+            break;
+        case MULTIPLY: eq->answer = eq->a * eq->b;
+            break;
+        case DIVIDE: eq->answer = eq->a / eq->b;
+    }
+}
 
 
 
@@ -60,14 +111,12 @@ void consumer_manager(int consumers) {
 int main(int argc, char* argv[]) {
     std::cout << "Hello, World!" << std::endl;
 
-    int queue_size = atoi(argv[1]);
+    auto queue_size = static_cast<unsigned int>(atoi(argv[1]));
     int operations = atoi(argv[2]);
     int num_producers = atoi(argv[3]);
     int num_consumers = atoi(argv[4]);
 
-    pool = queue(queue_size);
-
-
+    sem_init(&enqueue_semaphore, 0, queue_size);
 
     std::thread producer(producer_manager, num_producers);
     std::thread consumer(consumer_manager, num_consumers);
